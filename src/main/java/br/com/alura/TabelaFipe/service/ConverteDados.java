e675cf036cfb6ba40f2229fe156c7052b443d002
Giovana-Manuquian <gmanuquian@gmail.com>
2024-06-11 22:45:26 -0300

    projeto finalizado

--- src/main/java/br/com/alura/TabelaFipe/service/ConverteDados.java
+++ src/main/java/br/com/alura/TabelaFipe/service/ConverteDados.java
@@ -2,6 +2,7 @@ package br.com.alura.TabelaFipe.service; // Declara o pacote onde esta classe es
 
 import com.fasterxml.jackson.core.JsonProcessingException; // Importa a exceção que pode ser lançada ao processar JSON
 import com.fasterxml.jackson.databind.ObjectMapper; // Importa a classe principal da biblioteca Jackson para conversão entre JSON e objetos Java
+import com.fasterxml.jackson.databind.type.CollectionType;
 
 import java.util.List;
 
@@ -20,6 +21,13 @@ public class ConverteDados implements IConverteDados { // Define a classe públi
 
     @Override
     public <T> List<T> obterLista(String json, Class<T> classe) {
-        return null;
+        CollectionType lista = mapper.getTypeFactory()
+                .constructCollectionType(List.class, classe);
+
+        try {
+            return mapper.readValue(json, lista);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
